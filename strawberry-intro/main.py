aa44a43622fbbf6fc820a42e3da462756f63ac02
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-06-14 21:18:03 -0300

    Strawberry usage

--- strawberry-intro/main.py
+++ strawberry-intro/main.py
@@ -11,7 +11,7 @@ import strawberry
 
 engine = create_engine('sqlite:///database.db')
 
-class Pessoa(SQLModel, table=True):
+class PersonModel(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     age: int
@@ -25,28 +25,30 @@ class Person:
     age: int
 
 def create_app(age: int, name: str):
-    person = Person(name=name, age=age)
+    person = PersonModel(name=name, age=age)
 
     with Session(engine) as session:
         session.add(person)
         session.commit()
-        session.refresh()
+        session.refresh(person)
     
     return person
-
-@strawberry.type
-class Mutation:
-    create_app: Person = strawberry.field(resolver=create_app)
 
 @strawberry.type
 class Query:
 
     @strawberry.field
     def all_people(self) -> list[Person]:
-        query = select(Pessoa)
+        query = select(PersonModel)
         with Session(engine) as session:
             result = session.execute(query).scalars().all()
         return result
+    
+@strawberry.type
+class Mutation:
+    create_app: Person = strawberry.field(resolver=create_app)
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(
+    query=Query, 
+    mutation=Mutation)
 # send it with: http :8000/graphl query="{ allPeople { idade }}"
